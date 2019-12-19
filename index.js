1489195dff80cd7bac1ccf82983b85297f2997b1
davechai <davecqx@gmail.com>
2019-12-19 22:34:09 +0800

    responding to different routes

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const http = require('http')
 
+const users = require('./fixtures/users')
+const emails = require('./fixtures/emails')
+
 let server = http.createServer((req, res) => {
   let route  = req.method +  ' ' + req.url
-  res.end("You asked for" + route)
+
+  if(route === 'GET /users') {
+    res.end(JSON.stringify(users))
+  } else if (route === 'GET /emails') {
+    res.end(JSON.stringify(emails))
+  } else {
+    res.end("You asked for" + route)
+  }
 })
 
 server.listen(4002)
