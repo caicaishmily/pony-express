a381a4c52d022e9be6ffa70bfa9f6a34d68d08f9
davechai <davecqx@gmail.com>
2019-12-19 22:54:52 +0800

    express shorthands

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const http = require('http')
 const express = require('express')
 
 const users = require('./fixtures/users')
@@ -9,14 +8,12 @@ app.use((req, res) => {
   let route  = req.method +  ' ' + req.url
 
   if(route === 'GET /users') {
-    res.end(JSON.stringify(users))
+    res.send(users)
   } else if (route === 'GET /emails') {
-    res.end(JSON.stringify(emails))
+    res.send(emails)
   } else {
     res.end("You asked for" + route)
   }
 })
 
-let server = http.createServer(app)
-
-server.listen(4002)
+app.listen(4002)
