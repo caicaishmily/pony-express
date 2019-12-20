72b714b22e478801b82ce05d57e08f40164cc7e5
davechai <davecqx@gmail.com>
2019-12-20 22:28:54 +0800

    refact with route pattern

--- index.js
+++ index.js
@@ -4,16 +4,30 @@ const users = require('./fixtures/users')
 const emails = require('./fixtures/emails')
 
 let app = express()
+
+let getUsersRoute = (req, res) => {
+  res.send(users)
+}
+
+let getEmailsRoute = (req, res) => {
+  res.send(emails)
+}
+
+let routes = {
+  "GET /users": getUsersRoute,
+  "GET /emails": getEmailsRoute
+}
+
+let noRouteFound = (req, res) => {
+  let route = req.method + '' + req.url
+  res.end("You asked for " + route)
+}
+
 app.use((req, res) => {
   let route  = req.method +  ' ' + req.url
 
-  if(route === 'GET /users') {
-    res.send(users)
-  } else if (route === 'GET /emails') {
-    res.send(emails)
-  } else {
-    res.end("You asked for" + route)
-  }
+  let handler = routes[route] || noRouteFound
+  handler(req, res)
 })
 
-app.listen(4002)
+app.listen(4003)
