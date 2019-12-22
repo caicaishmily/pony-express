2db5e125f13f7e4793f9bc02d4ffc23b339111a0
davechai <davecqx@gmail.com>
2019-12-22 13:34:12 +0800

    first middleware contact

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const express = require('express')
 const userRouter = require('./routes/users')
 const emailsRouter = require('./routes/emails')
 
+const logger = require('./lib/logger')
+
 let app = express()
 
+app.use(logger)
 app.use("/users", userRouter)
 app.use("/emails", emailsRouter)
 
