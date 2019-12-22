832141b3150c5657173fe0e192b5e5a7dd7a2167
davechai <davecqx@gmail.com>
2019-12-22 12:19:15 +0800

    read request body

--- routes/emails.js
+++ routes/emails.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const emails = require('../fixtures/emails')
 
+const readBody = require('../lib/read-body')
+
 let getEmailsRoute = (req, res) => {
   res.send(emails)
 }
@@ -11,9 +13,15 @@ let getEmailRoute = (req, res) => {
   res.send(email)
 }
 
+let createEmailRoute = async (req, res) => {
+  let body = await readBody(req)
+  console.log(body.toString())
+}
+
 let emailsRouter = express.Router()
 
 emailsRouter.get('/', getEmailsRoute)
 emailsRouter.get('/:id', getEmailRoute)
+emailsRouter.post('/', createEmailRoute)
 
 module.exports = emailsRouter
