34d093cea636a5ad9009ca3c5bc4512300787c6d
davechai <davecqx@gmail.com>
2019-12-22 14:49:28 +0800

    serving a frontend

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 const compress = require('compression')
+const serveStatic = require('serve-static')
+const path = require('path')
 
 const userRouter = require('./routes/users')
 const emailsRouter = require('./routes/emails')
@@ -12,6 +14,7 @@ let logger = morgan('tiny')
 
 app.use(logger)
 app.use(compress(/*{ threshold: 0 }*/))
+app.use(serveStatic(path.join(__dirname, 'public')))
 app.use(notFoundHandler)
 app.use("/users", userRouter)
 app.use("/emails", emailsRouter)
